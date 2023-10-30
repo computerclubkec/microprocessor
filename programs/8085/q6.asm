834ac827183c2a101122a35cbd9d5ff97ca7b8d1
Gaurab Khanal <khanalgaurab98@gmail.com>
2023-10-30 12:38:51 +0545

    Counter C added

--- programs/8085/q6.asm
+++ programs/8085/q6.asm
@@ -2,6 +2,8 @@
 
 LXI H, 2500H  ; Load the memory location 2500H into the HL register pair.
 MVI A, 05H   ; Load the first 8-bit value (05H) into the accumulator (A).
+MVI C, 08H   ; Load the second 8-bit value (08H) into the counter (C).
+
 MOV B, A     ; Copy the value from A to B.
 MVI A, 00H   ; Initialize the accumulator (A) with 00H to store the result.
 
